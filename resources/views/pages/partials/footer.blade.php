f5522bd61ce902e169caba8c4587bab9316f4cf3
Ipan Suryadi <ipansuryadi@gmail.com>
2016-05-31 20:04:45 +0700

    commit ketiga, merubah footer link

--- resources/views/pages/partials/footer.blade.php
+++ resources/views/pages/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer elegant-color" if="footer" id="contact">
     <div class="footer-copyright">
         <div class="container">
-            <div class="col-md-10 col-md-offset-1 text-center"><a href="http://davidtrushkov.com/" target="_blank">© 2016 - PT Kemilau Indonesia</a></div>
+            <div class="col-md-10 col-md-offset-1 text-center"><a href="#" target="_blank">© 2016 - PT Kemilau Indonesia</a></div>
         </div>
     </div>
 </footer>
